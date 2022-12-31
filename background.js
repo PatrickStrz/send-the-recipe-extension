c215b03b137806685e7d3e90dac08a34da0e564c
Patrick Strzelec <patrick.strzelec@gmail.com>
2022-12-30 20:23:16 -0500

    clean up code

--- background.js
+++ background.js
@@ -7,14 +7,12 @@ console.log("CHROME", chrome)
 chrome.tabs.onActivated.addListener(function(activeInfo) {
   // console.log("tab activated:", activeInfo.tabId)
   // console.log("disabling")
-  chrome.action.disable();
+  chrome.action.disable()
 
   // console.log("sending message")
   chrome.tabs.sendMessage( activeInfo.tabId, {
     type: "TAB_ACTIVATED", payload: {}}, function(response) {
-    sendText(phone, response.status)
-    console.log(response.status);
-    alert(`SEENT$ ingredients to ${phone}: \n ------------- \n ${response.status}`)
+    console.log(response)
 })
 
 })
@@ -28,16 +26,11 @@ chrome.runtime.onMessage.addListener((msg, sender) => {
     
     if (msg.type === 'no-recipe') {
       console.log("setting popup to empty state")
-      chrome.action.disable();
+      chrome.action.disable()
       chrome.action.setPopup({ popup: 'emptyState.html'})
-
-      // Enable the page-action for the requesting tab.
-
-      // chrome.action.openPopup({tabId:sender.tab.id },);
     }
     if (msg.type === 'recipe') {
       console.log("recipe")
-      // new chrome.declarativeContent.ShowAction()
 
       chrome.action.enable()
       console.log("setting popup")
@@ -48,6 +41,5 @@ chrome.runtime.onMessage.addListener((msg, sender) => {
       console.log("sender:", sender)
       console.log("chrome:",chrome)
       console.log("action:", chrome.action)
-      // chrome.action.openPopup({tabId:sender.tab.id },);
     }
-  });
+  })
